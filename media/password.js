f798f243a4edeefb53b483d18149c250089bb70d
harru-P <69910800+harru-P@users.noreply.github.com>
2020-10-03 13:30:23 +0100

    Add files via upload

---
+++ media/password.js
@@ -0,0 +1,8 @@
+var text = document.getElementById("textbox");
+var button = document.getElementById("button");
+
+function on(){
+    if(text.value == "harru"){
+        window.location = "media/hidden.txt"
+    }
+}
